ce6d19cd8408fdb39776d9fc35a819b6ca54b5c7
dima <shymdima74@gmail.com>
2024-10-20 10:35:06 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('.tree');
+
+list.addEventListener('click', (e) => {
+  const element = e.target.closest('li');
+
+  if (element) {
+    Array.from(element.children).forEach((child) => {
+      if (child.tagName !== 'SPAN') {
+        child.style.display = child.style.display === 'none' ? '' : 'none';
+      }
+    });
+  }
+});
+
+document.body.addEventListener('click', (e) => {
+  if (!list.contains(e.target)) {
+    const allElements = document.querySelectorAll('.tree li ul');
+
+    allElements.forEach((ul) => {
+      ul.style.display = 'none';
+    });
+  }
+});
